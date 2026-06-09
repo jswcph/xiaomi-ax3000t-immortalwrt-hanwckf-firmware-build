8324241077b1b09fd8a9147ee78532f4f764ec6b
jswcph <84085592+jswcph@users.noreply.github.com>
2026-06-09 18:57:54 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -1,12 +1,19 @@
-# Description: (Before Update feeds)
+#!/bin/sh
+# 添加 passwall 源
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 
+# 添加 openclash 源
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
 
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加 argon 主题源
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master' >> feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加 iStore 源
+echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
 
-# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
-# echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+# 添加 lucky 源
+echo 'src-git lucky https://github.com/sirpdboy/luci-app-lucky.git' >> feeds.conf.default
+
+# 添加应用过滤源
+echo 'src-git appfilter https://github.com/destan19/OpenAppFilter.git' >> feeds.conf.default

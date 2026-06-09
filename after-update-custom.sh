6dbee45ce3330906e31ed7aac3edf2898acd7210
jswcph <84085592+jswcph@users.noreply.github.com>
2026-06-09 18:45:35 +0800

    Update after-update-custom.sh

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -2,8 +2,11 @@
 # Description: (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Set DHCP IP Address start and end
 sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
 sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
+
+# 修改路由器hostname
+sed -i "s/OpenWrt/OpenWrt/g" package/base-files/files/bin/config_generate
